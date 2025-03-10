59842ecad2863090525c849f074fec47c33e9bca
kira0826 <curaca492@gmail.com>
2025-03-10 08:42:34 -0500

    setting up lib test method to trigger test coverage.

---
+++ lib/utils/sum.dart
@@ -0,0 +1,6 @@
+
+// Sum function to test testing coverage
+
+int sum(int a, int b) {
+  return a + b;
+}
--- test/testing_test.dart
+++ test/testing_test.dart
@@ -1,7 +1,9 @@
+import 'package:block_qbit/utils/sum.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+
 void main() {
-  test('Example test', () {
-    expect(1 + 1, equals(2));
+  test('add function test', () {
+    expect(sum(1, 1) , equals(2)); // Ejecuta código de tu aplicación
   });
 }
